9693e0fe397255a4c8fb89d7c08f064b580622cb
Your Name <Mahmoudtat99@gmail.com>
2023-09-07 12:00:16 -0700

    How to be up to date in git

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,15 +4,15 @@
  * main - Entry point
  *
  * Description: print a code using write function
- * write(int fd.const void "buf.size_t count")
-
- * Retrun: 1 (not success)
- */
+ *   write(int fd.const void "buf.size_t count")
+ *
+ * Return: 1 (not success)
+*/
 
 int main(void)
 {
-char quo[] = "and that piece of art is useful\" - Dora Korpar , 2015-10-19\n";
-write(1,quo,59)
+char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, quo, 59)
 return (1);
 }
 
