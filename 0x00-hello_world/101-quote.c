d7239f6bba69c1c74d53fea5a4abda5783d104ac
Your Name <Mahmoudtat99@gmail.com>
2023-09-07 12:11:32 -0700

    How to be up to date in git

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * Description: print a code using write function
- *   write(int fd.const void *buf.size_t count)
+ * Description: print a quote using write function
+ *    ssize_t write(int fd.const void *buf, size_t count);
  *
- * Return: 1 (not success)
+ * Return: 1 (error).
 */
 
 int main(void)
