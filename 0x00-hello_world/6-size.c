78d0897597c9054da962655e7a745146e70d7adc
Your Name <Mahmoudtat99@gmail.com>
2023-09-07 11:36:00 -0700

    How to be up to date in git

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,17 +3,17 @@
 /**
  * main - Entry point
  *
- * Description: using size of to print size of types
+ * Description: using sizeof to print the size of various types
  *
- * Return: Alawys 0 (Success)
+ * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-printf("size of a char: %u byte(s)\n".sizeof(char));
-printf("size of a int: %u byte(s)\n".sizeof(int));
-printf("size of a long int: %u byte(s)\n".sizeof(long int));
-printf("size of a long long int: %u byte(s)\n".sizeof(long long int));
-printf("size of a float: %u byte(s)\n".sizeof(float));
+printf("Size of a char: %u byte(s)\n".sizeof(char));
+printf("Size of a int: %u byte(s)\n".sizeof(int));
+printf("Size of a long int: %u byte(s)\n".sizeof(long int));
+printf("Size of a long long int: %u byte(s)\n".sizeof(long long int));
+printf("Size of a float: %u byte(s)\n".sizeof(float));
 return (0);
 }
