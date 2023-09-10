15396ea7e8ee0b05b98cd8c8f2addc86e2d337b5
Your Name <Mahmoudtat99@gmail.com>
2023-09-10 09:59:45 -0700

    How to be up to date in git

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print numbers of base ten using putchar
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+int num = 0;
+while (num <= 9)
+{
+putchar(num + '0');
+num++;
+}
+putchar("\n");
+return (0);
+}
