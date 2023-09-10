291d21b96bb28c0f698bab20ffb8493ccf031282
Your Name <Mahmoudtat99@gmail.com>
2023-09-10 10:20:40 -0700

    How to be up to date in git

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print alphabet lowercase in reverse
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+char n = 'z';
+while (n >= 'a')
+{
+putchar(n);    
+n--;
+}
+putchar('\n');
+return (0);
+}
