ce8ac8c889b854faf28d2e654353fa4ffdfc782e
Your Name <Mahmoudtat99@gmail.com>
2023-09-12 11:16:15 -0700

    How to be up to date in git

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _islower - function to check if character is lowercase
+ * _islower - function to check if character 'c' is lowercase
  *
  *
- * 0c: checks input 'c' of function
+ * @c: checks input c of function
  *
- * Return: returns 1 if 'c' is lowercase
+ * Return: returns 1 if c is lowercase
  *   otherwise always 0 (success)
 */
 int _islower(int c)
