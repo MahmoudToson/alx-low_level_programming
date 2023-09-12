35e9202f0c254265567985612f95a05af77cad29
Your Name <Mahmoudtat99@gmail.com>
2023-09-12 06:00:31 -0700

    How to be up to date in git

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *print_alphabet - prints alphabets lowercase using __putchar
+ *
+*/
+void print_alphabet(void)
+{
+int i;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+}
