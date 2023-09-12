60833d5c67c00f261a3c1949762959e58fe7672d
Your Name <Mahmoudtat99@gmail.com>
2023-09-12 11:33:36 -0700

    How to be up to date in git

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - prints sign + if n is positive, - if negative and 0 if zero
+ *
+ * @n: takes integer type from function
+ *
+ * Return: returns 1 if postive, -1 if negative and 0 if 0
+ */
+int print_sign(int n)
+{
+int n;
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+}
