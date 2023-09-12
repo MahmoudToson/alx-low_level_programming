7ce240fb59615cb948ab859e966ca8d100ede881
Your Name <Mahmoudtat99@gmail.com>
2023-09-12 15:26:11 -0700

    k

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,43 +1,48 @@
 #include "main.h"
 
 /**
- *   print_times_table - print table n times
- *
- * @n: input
-*/
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
+ */
 void print_times_table(int n)
 {
-int i;
-int j;
-if (n <= 15 && n >= 0)
-{
-for (j = 0; j <= 9; j++)
-{
-_putchar(48);
-for (i = 1; i <= 9; i++)
-{
-_putchar(',');
-_putchar(' ');
-n = i * j;
-if (n <= 9)
-{
-_putchar(' ');
-}
-if (n <= 99)
-{
-_putchar(' ');
-}
-if (n >= 100)
-{
-_putchar((n / 100) + 48);
-_putchar((n / 10) % 10 + 48);
-}
-else if (n <= 99 && n >= 10)
+	int i, j, k;
 
-_putchar((n / 10) + 48);
-_putchar((n / 10) % 10 + 48);
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
-_putchar('\n');
-}
-}
-}
+
