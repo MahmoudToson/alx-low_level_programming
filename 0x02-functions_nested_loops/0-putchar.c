86792fb17574d7dcc1f5f04069a7d1b992c51412
Your Name <Mahmoudtat99@gmail.com>
2023-09-12 02:12:46 -0700

    How to be up to date in git

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print (__putchar)
+ *
+ * Return: always 0 (success)
+*/
+int main(void)
+{
+printf("_putchar");
+printf("\n");
+return (0);
+}
